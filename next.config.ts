ae1afba105099cad696f6107de6bf5acbe96fa3b
Anyon-Aida <kovacszalan23@gmail.com>
2025-11-17 14:11:11 +0100

    link

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         { protocol: "https", hostname: "lh4.googleusercontent.com" },
         { protocol: "https", hostname: "lh5.googleusercontent.com" },
         { protocol: "https", hostname: "lh6.googleusercontent.com" },
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          pathname: '/dfagrsgsz/**',
+        },
       ],
     },
     eslint: {
